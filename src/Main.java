7b57fd3ae3b2bec7969cb372c0292342249b1ea0
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-09-07 17:10:58 +0300

    Написали код программы

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+package src;
+
+import InMemoryModel.*;
+import ModelElements.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Создаем экземпляр наблюдателя изменений модели
+        IModelChangedObserver observer = new IModelChangedObserver() {
+            @Override
+            public void applyUpdateModel() {
+                // Реализация обновления модели при изменениях
+                System.out.println("Модель была изменена.");
+            }
+        };
+
+        // Создаем хранилище модели с передачей наблюдателя изменений
+        ModelStore modelStore = new ModelStore(observer);
+
+        // Производим операции с объектами модели
+        Angle3D angle = new Angle3D();
+        Point3D point = new Point3D();
+        modelStore.cameras.rotate(angle);
+        modelStore.cameras.move(point);
+        modelStore.flashes.rotate(angle);
+        modelStore.flashes.move(point);
+
+        // Выводим информацию о модели
+        Scene scene = modelStore.getScene(1);
+        System.out.println("ID сцены: " + scene.id);
+
+        // Вызываем метод уведомления о изменении модели
+        modelStore.notifyChange(modelStore);
+    }
+}
